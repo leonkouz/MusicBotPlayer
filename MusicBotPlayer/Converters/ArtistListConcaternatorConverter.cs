7d8755a4be50e3dfdad2e59cd5fbfde23a151d56
Leon <leon.kouz@hotmail.com>
2018-11-15 10:34:00 +1100

    Added columns to album search

---
+++ MusicBotPlayer/Converters/ArtistListConcaternatorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MusicBotPlayer
+{
+    public class ArtistListConcaternatorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var artistsList = (List<ArtistSimplified>)value;
+
+            string artistConcat = String.Empty;
+
+            foreach(ArtistSimplified artist in artistsList)
+            {
+                artistConcat += artist.name + ", ";
+            }
+
+            // Trim the whitespaces.
+            artistConcat.Trim();
+
+            // Remove the last comma from the string.
+            string trimmedString = artistConcat.Remove(artistConcat.Length - 2, 2);
+
+            return trimmedString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MusicBotPlayer/Converters/SelectImageConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MusicBotPlayer
+{
+    public class SelectImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var imageList = (List<Image>)value;
+
+            if(imageList.Count != 0)
+            {
+                // Return the last image from the list which is 64 x x64
+                return imageList.Last().url;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
